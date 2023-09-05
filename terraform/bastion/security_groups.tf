562bb4735886ff1be59bfcf075396944335dcdca
Sarath Chandra Vidya Sagar Machupalli <vidyasagar.msc@ibm.com>
2023-09-05 17:27:36 -0500

    Add bastion

---
+++ terraform/bastion/security_groups.tf
@@ -0,0 +1,43 @@
+locals {
+
+  security_group_rule_bastion_object = {
+    for rule in var.bastion_sg_rules :
+    rule.name => rule
+  }
+}
+resource "ibm_is_security_group" "bastion_sg" {
+  name           = "${var.prefix}-edge-bastion-sg"
+  vpc            = var.vpc_id
+  resource_group = var.resource_group_id
+}
+
+resource "ibm_is_security_group_rule" "bastion_sg_rules" {
+  for_each  = local.security_group_rule_bastion_object
+  group     = ibm_is_security_group.bastion_sg.id
+  direction = each.value.direction
+  remote    = each.value.remote
+
+  dynamic "tcp" {
+    for_each = each.value.tcp == null ? [] : [each.value]
+    content {
+      port_min = each.value.tcp.port_min
+      port_max = each.value.tcp.port_max
+    }
+  }
+
+  dynamic "udp" {
+    for_each = each.value.udp == null ? [] : [each.value]
+    content {
+      port_min = each.value.udp.port_min
+      port_max = each.value.udp.port_max
+    }
+  }
+
+  dynamic "icmp" {
+    for_each = each.value.icmp == null ? [] : [each.value]
+    content {
+      type = each.value.icmp.type
+      code = each.value.icmp.code
+    }
+  }
+}
---
+++ terraform/bastion/variables.tf
@@ -0,0 +1,135 @@
+variable "ibm_cloud_region" {
+  type        = string
+  description = "IBM Cloud region to provision the resources."
+  default     = "us-south"
+}
+
+variable "prefix" {
+  type        = string
+  default     = "ha-bastion"
+  description = "The string that needs to be attached to every resource created"
+}
+
+variable "resource_group_id" {
+  type        = string
+  description = "The resource group where all the resources will be provisioned."
+}
+
+variable "vpc_id"{
+  type        = string
+  description = "VPC id"
+}
+
+variable "vpc_subnets" {
+    type = any
+    description = "List of subnets"
+}
+
+##################################################################################
+# BASTION variables
+##################################################################################
+
+variable "vsi_bastion_image" {
+  type        = string
+  default     = "ibm-redhat-8-6-minimal-amd64-4"
+  description = "To see the available images run ibmcloud is images command"
+}
+
+variable "vsi_bastion_profile" {
+  type        = string
+  default     = "bx2-2x8"
+  description = "To see the available images run ibmcloud is images command"
+}
+
+variable "bastion_sg_rules" {
+  type = list(
+    object({
+      name      = string
+      direction = string
+      remote    = string
+      tcp = optional(
+        object({
+          port_max = optional(number)
+          port_min = optional(number)
+        })
+      )
+      udp = optional(
+        object({
+          port_max = optional(number)
+          port_min = optional(number)
+        })
+      )
+      icmp = optional(
+        object({
+          type = optional(number)
+          code = optional(number)
+        })
+      )
+    })
+  )
+
+  default = [
+    {
+      "direction" : "inbound",
+      "name" : "allow-inbound-bastion",
+      "remote" : "192.168.0.0/16"
+    },
+    {
+      "direction" : "inbound",
+      "name" : "allow-inbound-from-mirror",
+      "remote" : "161.26.0.0/16"
+    },
+    {
+      "direction" : "inbound",
+      "name" : "allow-inbound-bastion-ssh",
+      "remote" : "10.10.128.0/24",
+      "tcp" : {
+        "port_max" : 22,
+        "port_min" : 22
+      }
+    },
+    {
+      "direction" : "inbound",
+      "name" : "allow-inbound-bastion-icmp",
+      "remote" : "10.10.128.0/24",
+      icmp : {
+        type : 8
+        code : null
+      }
+    },
+    {
+      "direction" : "inbound",
+      "name" : "allow-inbound-ase-ssh",
+      "remote" : "10.10.0.0/17",
+      "tcp" : {
+        "port_max" : 22,
+        "port_min" : 22
+      }
+    },
+    {
+      "direction" : "inbound",
+      "name" : "allow-inbound-ase-icmp",
+      "remote" : "10.10.0.0/17",
+      icmp : {
+        type : 8
+        code : null
+      }
+    },
+    {
+      "direction" : "outbound",
+      "name" : "allow-outbound-bastion",
+      "remote" : "192.168.0.0/16"
+    },
+    {
+      "direction" : "outbound",
+      "name" : "allow-outbound-ase-host",
+      "remote" : "10.10.128.0/24"
+    },
+    
+    {
+      "direction" : "outbound",
+      "name" : "allow-outbound-mirror",
+      "remote" : "161.26.0.0/16"
+    },
+  ]
+}
