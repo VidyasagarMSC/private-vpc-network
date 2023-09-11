a285946fe6e584908f90a52c2fdcd719569c27ef
Sarath Chandra Vidya Sagar Machupalli <vidyasagar.msc@ibm.com>
2023-09-11 16:10:40 -0500

    Added security group rules to allow 9090 :fire:

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -13,7 +13,7 @@ resource "ibm_is_security_group" "vsi_sg" {
 }
 
 resource "ibm_is_security_group_rule" "vsi_sg_rules" {
-  for_each  =  local.security_group_rule_private_vsi_object
+  for_each  = local.security_group_rule_private_vsi_object
   group     = ibm_is_security_group.vsi_sg.id
   direction = each.value.direction
   remote    = each.value.remote
@@ -40,5 +40,31 @@ resource "ibm_is_security_group_rule" "vsi_sg_rules" {
       type = each.value.icmp.type
       code = each.value.icmp.code
     }
+  }
+}
+
+###########################################################################
+# Security group rules to allow inbound and outbound traffic on port 9090
+# for session recording web console
+###########################################################################
+resource "ibm_is_security_group_rule" "allow_9090_inbound" {
+  count     = var.allow_port_9090 ? 1 : 0
+  group     = ibm_is_security_group.vsi_sg.id
+  direction = "inbound"
+  remote    = "0.0.0.0"
+  tcp {
+    port_min = 9090
+    port_max = 9090
+  }
+}
+
+resource "ibm_is_security_group_rule" "allow_9090_outbound" {
+  count     = var.allow_port_9090 ? 1 : 0
+  group     = ibm_is_security_group.vsi_sg.id
+  direction = "outbound"
+  remote    = "0.0.0.0"
+  tcp {
+    port_min = 9090
+    port_max = 9090
   }
 }
