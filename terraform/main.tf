88290173f762e687462ec59b764cb640a9af7360
Sarath Chandra Vidya Sagar Machupalli <vidyasagar.msc@ibm.com>
2023-09-11 16:09:52 -0500

    ACLS terraform fmt :rocket:

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,20 +3,21 @@ resource "ibm_resource_group" "group" {
 }
 
 module "bastion" {
-  source = "./bastion"
-  vpc_id = ibm_is_vpc.vpc.id
-  subnets = ibm_is_subnet.subnets
-  prefix = var.prefix
+  source            = "./bastion"
+  vpc_id            = ibm_is_vpc.vpc.id
+  subnets           = ibm_is_subnet.subnets
+  prefix            = var.prefix
   resource_group_id = ibm_resource_group.group.id
-  ibm_cloud_region = var.ibm_cloud_region
+  ibm_cloud_region  = var.ibm_cloud_region
 }
 
 module "vpn" {
-  source = "./vpn"
-  vpc_id = ibm_is_vpc.vpc.id
-  vpc_subnets = ibm_is_subnet.subnets
-  prefix = var.prefix
-  resource_group_id = ibm_resource_group.group.id
-  ibm_cloud_region = var.ibm_cloud_region
+  source                          = "./vpn"
+  vpc_id                          = ibm_is_vpc.vpc.id
+  vpc_subnets                     = ibm_is_subnet.subnets
+  prefix                          = var.prefix
+  resource_group_id               = ibm_resource_group.group.id
+  ibm_cloud_region                = var.ibm_cloud_region
   secrets_manager_certificate_crn = var.secrets_manager_certificate_crn
+  allow_port_9090                 = var.allow_port_9090
 }
